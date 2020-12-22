3adae8f2b3fe4827273df17049678777d71ca764
Sanja Wetzel <sanjwich@gmail.com>
2020-12-22 20:06:43 +0100

    props is confusing af

--- part1/ex.1.1/src/index.js
+++ part1/ex.1.1/src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+const Header = ({ course }) => <div><h1>{course}</h1></div>
 
 const Content = (props) => {
   return (
     <div>
-      <p>{'Fundamentals of React'} {10}</p>
-      <p>{'Using props to pass data'} {7}</p>
-      <p>{'State of a component'} {14}</p>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
     </div>
   )
 }
@@ -22,34 +16,28 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Total number of exercises: {10 + 7 + 14}</p>
+      <p>Total number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
+    <>
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+        <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
+    </>
   )
 }
 
