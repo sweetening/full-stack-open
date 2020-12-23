ccef409dd754f33251aefeb79327423c2edf984e
Sanja Wetzel <sanjwich@gmail.com>
2020-12-23 12:30:56 +0100

    got the first part of the app up and running, nice

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        Good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        Neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        Bad
+      </button>
+
+      <h1>Statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+);
